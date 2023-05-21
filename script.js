b66c86d10e471113710487e4808547155f187437
lovegupta112 <loveguptaofficials@gmail.com>
2023-05-21 20:29:53 +0530

    add id

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
 
 let div=document.createElement('div');
+div.id="browser-info";
 let para=document.createElement('p');
 
 para.innerText=`You are using ${window.navigator.appName} version ${window.navigator.appVersion}`;

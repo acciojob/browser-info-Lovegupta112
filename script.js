0e5023c69e0b818de69f88ca5523eadb03bfc15d
lovegupta112 <loveguptaofficials@gmail.com>
2023-05-21 20:24:15 +0530

    added browser info

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let div=document.createElement('div');
+let para=document.createElement('p');
+
+para.innerText=`You are using ${window.navigator.appName} version ${window.navigator.appVersion}`;
+
+div.append(para);
+document.body.append(div);
+console.log(para.innerText);
